08330389f72e996a97971dd00407829f247fb0b6
Juan Esteban Caicedo Valencia <100038518+juancaicedo10@users.noreply.github.com>
2024-06-08 12:35:11 -0500

    Delete backend directory

--- backend/utils/NumeroVentas.js
+++
@@ -1,7 +0,0 @@
-const sql = require('mssql');
-
-module.exports = async function generarNumeroVentas() {
-    const pool = await sql.connect();
-    const result = await pool.request().query('SELECT COUNT(*) FROM Financiero.Ventas');
-    return result.recordset[0][''].toString().padStart(6, '0');
-}
