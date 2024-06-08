754e1599774f4cb869b03b31a3fb2b05e673fca8
Juan Esteban Caicedo Valencia <100038518+juancaicedo10@users.noreply.github.com>
2024-06-08 14:42:49 -0500

    Delete backend directory

--- backend/routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');                
-const { login } = require('../controller/sesionCtrls/loginCtrl.js');
-
-const router = express.Router();
-
-router.post('/', login)
-
-module.exports = router;
